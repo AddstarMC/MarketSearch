e2eb77ff05113bf98a020de09e6cb06c95884813
Narimm <benjicharlton@gmail.com>
2019-04-29 21:16:48 +1000

    Update for Plotsquared and uskyblock api

--- src/au/com/addstar/marketsearch/PlotProviders/PlotSquaredPlotProvider.java
+++ src/au/com/addstar/marketsearch/PlotProviders/PlotSquaredPlotProvider.java
@@ -1,11 +1,12 @@
 package au.com.addstar.marketsearch.PlotProviders;
 
-import com.intellectualcrafters.plot.api.PlotAPI;
-import com.intellectualcrafters.plot.object.Plot;
-import com.intellectualcrafters.plot.object.PlotPlayer;
-import com.plotsquared.bukkit.util.BukkitUtil;
+
+import com.github.intellectualsites.plotsquared.api.PlotAPI;
+import com.github.intellectualsites.plotsquared.plot.PlotSquared;
+import com.github.intellectualsites.plotsquared.plot.object.Plot;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
+
+import com.github.intellectualsites.plotsquared.plot.object.Location;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
@@ -24,17 +25,32 @@ public class PlotSquaredPlotProvider implements PlotProvider {
     }
 
     @Override
-    public String getPlotOwner(Location location) {
-        Plot plot =  api.getPlot(location);
+    public String getPlotOwner(org.bukkit.Location location) {
+        Plot plot = getPlot(location);
         if(plot.getOwners().size() == 1){
             UUID uuid = plot.getOwners().iterator().next();
                 return Bukkit.getOfflinePlayer(uuid).getName();
         }
         return null;
     }
-    
-    public void gotoPlot(Player player, Location loc){
-        Plot plot = api.getPlot(loc);
-        player.teleport(BukkitUtil.getLocation(plot.getHome()));
+
+    private Plot getPlot(org.bukkit.Location location) {
+        if (location == null || location.getWorld() == null)
+            return null;
+        Location location1 = new Location(
+                location.getWorld().getName(),
+                location.getBlockX(),
+                location.getBlockY(),
+                location.getBlockZ(),
+                location.getYaw(),
+                location.getPitch());
+        return location1.getPlot();
+    }
+
+    public void gotoPlot(Player player, org.bukkit.Location loc) {
+        Plot plot = getPlot(loc);
+        Location location = plot.getHome();
+        org.bukkit.Location tpLoc = new org.bukkit.Location(Bukkit.getWorld(location.getWorld()), location.getX(), location.getY(), location.getZ());
+        player.teleport(tpLoc);
     }
 }
